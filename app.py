a0b4ee4c873331e5357d6b2b71a9893ef6a2ebb2
Bernardo Bicalho <bbicalho@gmail.com>
2023-08-15 09:32:23 -0300

    respect line breaks in gpt formatting and set temperature to zero

--- app.py
+++ app.py
@@ -477,6 +477,7 @@ def answer_question(
         response = openai.ChatCompletion.create(
             # model="gpt-3.5-turbo",
             model="gpt-3.5-turbo-16k",
+            temperature=0.0,
             messages=[
                 {"role": "system", "content": prompt},
                 {"role": "user", "content": question}
